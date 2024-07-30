be7945de43b27a6b709961415f3b464d8360732a
G-OMONDI <112931518+G-OMONDI@users.noreply.github.com>
2024-07-30 19:54:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 console.log('JavaScript is linked successfully!');
+// String
+let myString = "Hello, JavaScript!";
+
+// Number
+let myNumber = 42;
+
+// Boolean
+let myBoolean = true;
+
+// Null
+let myNull = null;
+
+// Undefined
+let myUndefined;
+
+// Object
+let myObject = {
+    name: "Glen",
+    age: 30,
+    isStudent: false
+};
+
+// Symbol
+let mySymbol = Symbol('mySymbol');
+
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log(myNull);
+console.log(myUndefined);
+console.log(myObject);
+console.log(mySymbol);
+
